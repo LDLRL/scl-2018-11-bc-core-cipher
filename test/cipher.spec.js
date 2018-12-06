2412fa6020c64ba9f1399a44134933a6f41a4ce1
LDLRL <lomelek@gmail.com>
2018-12-06 11:56:52 -0300

    se modifico el formato del index.js para que seaaceptado por las funciones

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,25 +1,37 @@
-describe('cipher', () => {
-
+describe('cipher', () => {  
   it('debería ser un objeto', () => {
-    assert.equal(typeof cipher, 'object');
-  });
 
-  describe('cipher.encode', () => {
+  assert.equal(typeof cipher, 'object');
 
+});  
+
+describe('cipher.encode', () => {    
+  it('debería ser una función', () => {
+    assert.equal(typeof cipher.encode, 'function');
+
+  });   
+  
+  it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', ()=> {      
+    assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+  }  
+  );  
+  
+  describe('cipher.decode', () => {    
     it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
-    });
+    assert.equal(typeof cipher.decode, 'function');
+  });    
+  it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', ()=> {      
+    assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");  });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
-  });
 
-  describe('cipher.decode', () => {
-
-    it('debería ser una función', () => {
+    describe('cipher.decode lowerCase', () => {    
+      it('debería ser  lowerCase', () => {
       assert.equal(typeof cipher.decode, 'function');
-    });
-
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
-  });
-
-});
+    });    
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', ()=> {      
+      assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33), "abcdefghijklmnopqrstuvwxyz" );  });
+    
+    })  
+})
+})
+})
