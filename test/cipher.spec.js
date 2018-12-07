7818e71702f607249fac9488ec30b8632a3f3ec7
LDLRL <lomelek@gmail.com>
2018-12-07 08:22:20 -0300

    subiendo ultimos cambios en readme, index y style

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -31,7 +31,15 @@ describe('cipher.encode', () => {
     it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', ()=> {      
       assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33), "abcdefghijklmnopqrstuvwxyz" );  });
     
-    })  
+    describe('cipher.decode lowerCase', () => {    
+      it('debería ser  lowerCase', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });    
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', ()=> {      
+      assert.equal(cipher.encode("abcdefghijklmnopqrstuvwxyz", 33), "hijklmnopqrstuvwxyzabcdefg");  });
+
+})
+})  
 })
 })
 })
