265b2bf9c68fcdb6be98ed13711d5d3a84bb6416
LDLRL <lomelek@gmail.com>
2018-12-06 21:30:50 -0300

    correccion de css. inicio del readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,57 @@
 window.cipher = {
-  encode: () => {
+  encode: (texto, offset) => {
     /* Acá va tu código */
-  },
-  decode: () => {
+
+    // function encode(texto, offset){
+
+    let result = "";
+    for (let letra of texto) {
+        let valorAscii = letra.charCodeAt();
+        if (valorAscii >= 65 && valorAscii <= 90) {
+            let calc = ((valorAscii - 65 + offset) % 26) + 65;
+            if (calc < 65) {
+                calc += 26;
+            }
+            result += String.fromCharCode(calc);
+        } else if (valorAscii >= 97 && valorAscii <= 122) {
+            let calc = (valorAscii - 97 + offset) % 26 + 97;
+            if (calc < 97) {
+                calc += 26;
+            }
+            result += String.fromCharCode(calc);
+        } else {
+            result += letra;
+        }
+    }
+    return result;
+},
+
+
+
+
+decode: (texto, offset) => {
     /* Acá va tu código */
-  }
-};
+    // function decode(texto, offset){
+
+    let result = "";
+    for (let letra of texto) {
+        let valorAscii = letra.charCodeAt();
+        if (valorAscii >= 65 && valorAscii <= 90) { //MAYUSCULAS
+            let calc = ((valorAscii - 65 - offset) % 26) + 65;
+            if (calc < 65) {
+                calc += 26;
+            }
+            result += String.fromCharCode(calc);
+        } else if (valorAscii >= 97 && valorAscii <= 122) { //MINUSCULAS
+            let calc = (valorAscii - 97 - offset) % 26 + 97;
+            if (calc < 97) {
+                calc += 26;
+            }
+            result += String.fromCharCode(calc);
+        } else {
+            result += letra;
+        }
+    }
+    return result;
+}
+}
