98513f0a8bc87733f9b0d684796f84d3190102e9
LDLRL <lomelek@gmail.com>
2018-12-07 08:45:36 -0300

    actualizacion del test

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -24,20 +24,37 @@ describe('cipher.encode', () => {
     assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");  });
 
 
-    describe('cipher.decode lowerCase', () => {    
-      it('debería ser  lowerCase', () => {
-      assert.equal(typeof cipher.decode, 'function');
-    });    
-    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', ()=> {      
-      assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33), "abcdefghijklmnopqrstuvwxyz" );  });
-    
-    describe('cipher.decode lowerCase', () => {    
-      it('debería ser  lowerCase', () => {
-      assert.equal(typeof cipher.encode, 'function');
-    });    
-    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', ()=> {      
-      assert.equal(cipher.encode("abcdefghijklmnopqrstuvwxyz", 33), "hijklmnopqrstuvwxyzabcdefg");  });
+  describe('cipher.decode lowerCase', () => {    
+    it('debería decodificar en lowerCase', () => {
+    assert.equal(typeof cipher.decode, 'function');
+  });    
+  it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', ()=> {      
+    assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33), "abcdefghijklmnopqrstuvwxyz" );  });
+  
+  describe('cipher.encode lowerCase', () => {    
+    it('debería codificar en lowerCase', () => {
+    assert.equal(typeof cipher.encode, 'function');
+  });    
+  it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', ()=> {      
+    assert.equal(cipher.encode("abcdefghijklmnopqrstuvwxyz", 33), "hijklmnopqrstuvwxyzabcdefg"); 
+  });
 
+  describe('cipher.encode negativo', () => {    
+    it('debería codificar con numero negativo al usar minúsculas', () => {
+    assert.equal(typeof cipher.encode, 'function');
+  });    
+  it('debería retornar "xyzabcdefghijklmnopqrstuvw" para "abcdefghijklmnopqrstuvwxyz" con offest -3', ()=> {      
+    assert.equal(cipher.encode("abcdefghijklmnopqrstuvwxyz", -3), "xyzabcdefghijklmnopqrstuvw");  });
+    
+
+  describe('cipher.encode negativo', () => {    
+    it('debería codificar con numero negativo al usar mayúsculas', () => {
+    assert.equal(typeof cipher.encode, 'function');
+  });    
+  it('debería retornar "XYZABCDEFGHIJKLMNOPQRSTUVW" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest -3', ()=> {      
+    assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", -3), "XYZABCDEFGHIJKLMNOPQRSTUVW");  });
+})
+})
 })
 })  
 })
